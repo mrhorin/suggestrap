40ff53aaf4f5910318c6864a56333ecf84d720d8
mrhorin <u0813mb@gmail.com>
2017-10-08 09:40:36 +0900

    Remove sample files

--- src/sample/sample.js
+++
@@ -1,9 +0,0 @@
-import Suggestrap from 'js/suggestrap'
-
-const req = {
-  target: "target",
-  url: "http://sample.org/json/%QUERY",
-  key: "name",
-}
-
-var s = new Suggestrap(req)
--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,6 @@ module.exports = {
   entry: {
     'suggestrap': './src/js/suggestrap.js',
     './test/browser-test': './src/test/browser-test.js',
-    './sample/sample': './src/sample/sample.js'
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
@@ -22,11 +21,6 @@ module.exports = {
   ],
   plugins: [
     new WebpackNotifierPlugin(),
-    new HtmlWebpackPlugin({
-      filename: './sample/index.html',
-      template: './src/sample/index.pug',
-      chunks: ['./sample/sample'],
-    }),
     new HtmlWebpackPlugin({
       filename: './test/index.html',
       template: './src/test/index.pug',
